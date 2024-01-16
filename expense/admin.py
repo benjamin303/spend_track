1e720323a00255d822a0d908e6d5c9a4f52c778c
benjamin303 <benjamin.novak42@gmail.com>
2024-01-16 22:18:49 +0100

    add models to admin

--- expense/admin.py
+++ expense/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Expense, MethodOfPayment
+
 
 # Register your models here.
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("amount", "methodofpayment", "category", "subcategory", "date")
+admin.site.register(Expense, ExpenseAdmin)
+
+admin.site.register(MethodOfPayment)
+
