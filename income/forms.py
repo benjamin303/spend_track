a0149fa4e388d6e2ac83623b3791ca44d6f8c68e
benjamin303 <benjamin.novak42@gmail.com>
2024-06-18 19:06:40 +0200

    edit income styles

--- income/forms.py
+++ income/forms.py
@@ -1,7 +1,20 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Income
 
 class IncomeForm(ModelForm):
     class Meta:
         model = Income
-        fields = '__all__'
+        fields = ["category", "methodofincome", "amount", "description"]
+        widgets = {
+            "category": forms.Select(attrs={"class": "form-select"}),
+            "subcategory": forms.Select(attrs={"class": "form-select"}),
+            "methodofincome": forms.Select(attrs={"class": "form-select"}),
+            "amount": forms.NumberInput(attrs={"class": "form-control"}),
+            "description": forms.Textarea(attrs={"class": "form-control", "rows": 3}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Set the default value for methodofincome
+        self.fields["methodofincome"].initial = 1  # default value is card
